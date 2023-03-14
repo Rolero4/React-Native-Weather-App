1234490a763e3867d1c1afe7cd3526710f4bbbd2
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-03-14 15:28:46 +0100

    Toggler info badge

--- components/Header/headerComponents/Toggler.js
+++ components/Header/headerComponents/Toggler.js
@@ -1,9 +1,13 @@
-import { View, Switch, StyleSheet } from "react-native";
-import React, { useState } from "react";
+import { View, Text, Switch, StyleSheet } from "react-native";
 
 export default function Toggler({ callback, state }) {
   return (
     <View style={styles.container}>
+      {!state ? (
+        <Text style={styles.focus}>Focus Mode</Text>
+      ) : (
+        <Text style={styles.simple}>Simple Mode</Text>
+      )}
       <Switch
         trackColor={{ false: "#767577", true: "#81b0ff" }}
         thumbColor={state ? "#f5dd4b" : "#f4f3f4"}
@@ -19,5 +23,15 @@ const styles = StyleSheet.create({
   container: {
     width: "30%",
     alignItems: "center",
+  },
+
+  focus: {
+    color: "green",
+    margin: 5,
+  },
+
+  simple: {
+    color: "lightgreen",
+    margin: 5,
   },
 });
